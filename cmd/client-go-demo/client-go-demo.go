131ddb9b246718e6a5c44b6330bc57c7fc17daad
richardgu <richardgu@tencent.com>
2021-07-16 19:51:21 +0800

    add client-go demo

--- cmd/client-go-demo/client-go-demo.go
+++ cmd/client-go-demo/client-go-demo.go
@@ -2,22 +2,51 @@ package main
 
 import (
 	"context"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
+	"net"
 )
 
 func main() {
-	kubeconfig := "./config"
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		klog.Fatalf("BuildConfigFromFlags failed, err: %s", err)
+	{
+		kubeconfig := "./config"
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			klog.Fatalf("BuildConfigFromFlags failed, err: %s", err)
+		}
+		clientset := clientset.NewForConfigOrDie(config)
+		podList, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			klog.Fatalf("Pods List failed, err: %s", err)
+		}
+		klog.Infof("podlist-v1: %v", podList)
 	}
-	clientset := clientset.NewForConfigOrDie(config)
-	podList, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		klog.Fatalf("Pods List failed, err: %s", err)
+
+	{
+		rootCAFile := "./ca.crt"
+		tlsClientConfig := rest.TLSClientConfig{}
+		tlsClientConfig.CAFile = rootCAFile
+		host := "xxxxx"
+		port := "xxx"
+		tokenFile := "./token"
+		config := &rest.Config{
+			Host:            "https://" + net.JoinHostPort(host, port),
+			TLSClientConfig: tlsClientConfig,
+			BearerTokenFile: tokenFile,
+		}
+		clientset, err := clientset.NewForConfig(config)
+		if err != nil {
+			klog.Fatalf("NewForConfig failed, err: %s", err)
+		}
+		podList, err := clientset.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			klog.Fatalf("Pods List failed, err: %s", err)
+		}
+		klog.Infof("podlist-v2: %v", podList)
 	}
-	klog.Infof("podlist: %v", podList)
+	klog.Infof(v1.NamespaceDefault)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/pkg/errors v0.9.1
+	k8s.io/api v0.21.3
 	k8s.io/apimachinery v0.21.3
 	k8s.io/client-go v0.21.3
 	k8s.io/klog/v2 v2.10.0
